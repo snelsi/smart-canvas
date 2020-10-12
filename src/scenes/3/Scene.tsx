d5741b0b1f82f566620ea770a6c03112ca96b14e
Roman Zhuravlov <SnelsiThePirate@gmail.com>
2020-10-12 10:33:15 +0300

    Refactor scene 3

--- src/scenes/3/Scene.tsx
+++ src/scenes/3/Scene.tsx
@@ -15,21 +15,18 @@ const WorldTransform = ({ children }) => {
   const [Szy] = useField<number>(`${prefix}Szy`);
   const [Sxz] = useField<number>(`${prefix}Sxz`);
   const [Syz] = useField<number>(`${prefix}Syz`);
-  const SyxRef = React.useRef<number>(0);
-  const SzxRef = React.useRef<number>(0);
-  const SxyRef = React.useRef<number>(0);
-  const SzyRef = React.useRef<number>(0);
-  const SxzRef = React.useRef<number>(0);
-  const SyzRef = React.useRef<number>(0);
+  const prev = React.useRef({
+    prevSyx: 0,
+    prevSzx: 0,
+    prevSxy: 0,
+    prevSzy: 0,
+    prevSxz: 0,
+    prevSyz: 0,
+  });
 
-  const geometryRef = useUpdate<THREE.BoxGeometry>(
-    (geometry) => {
-      const prevSyx = SyxRef.current;
-      const prevSzx = SzxRef.current;
-      const prevSxy = SxyRef.current;
-      const prevSzy = SzyRef.current;
-      const prevSxz = SxzRef.current;
-      const prevSyz = SyzRef.current;
+  const meshRef = useUpdate<THREE.Mesh>(
+    (mesh) => {
+      const { prevSyx, prevSzx, prevSxy, prevSzy, prevSxz, prevSyz } = prev.current;
 
       const reverseMatrix = new THREE.Matrix4();
       reverseMatrix.set(
@@ -50,31 +47,32 @@ const WorldTransform = ({ children }) => {
         0,
         1,
       );
-      geometry.applyMatrix4(reverseMatrix);
+      mesh.applyMatrix4(reverseMatrix);
 
-      SyxRef.current = Syx;
-      SzxRef.current = Szx;
-      SxyRef.current = Sxy;
-      SzyRef.current = Szy;
-      SxzRef.current = Sxz;
-      SyzRef.current = Syz;
+      mesh.position.set(0, 0, 0);
+      mesh.rotation.set(0, 0, 0);
+      mesh.scale.set(1, 1, 1);
+      mesh.updateMatrix();
+
+      prev.current = {
+        prevSyx: Syx,
+        prevSzx: Szx,
+        prevSxy: Sxy,
+        prevSzy: Szy,
+        prevSxz: Sxz,
+        prevSyz: Syz,
+      };
 
       const matrix = new THREE.Matrix4();
 
       matrix.set(1, Syx, Szx, 0, Sxy, 1, Szy, 0, Sxz, Syz, 1, 0, 0, 0, 0, 1);
 
-      geometry.applyMatrix4(matrix);
+      mesh.applyMatrix4(matrix);
     },
     [Syx, Szx, Sxy, Szy, Sxz, Syz],
   );
 
-  return (
-    <mesh>
-      <boxGeometry ref={geometryRef} />
-      <meshBasicMaterial wireframe={true} />
-      {children}
-    </mesh>
-  );
+  return <mesh ref={meshRef}>{children}</mesh>;
 };
 
 export const Figure = () => {
